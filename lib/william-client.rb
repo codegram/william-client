1969565d8f0980f8336cbb25bef683fd4b64be08
Marc Divins <marcdivc@gmail.com>
2012-08-22 15:16:12 +0200

    Finish basic requests to william and add docs

--- lib/william-client.rb
+++ lib/william-client.rb
@@ -1,27 +1,82 @@
 require "william-client/version"
+require "william-client/config"
 require 'hyperclient'
 
 module William
   class Client
     include Hyperclient
 
-    entry_point 'http://localhost:3000'
+    entry_point William.config.william_api_url
     http_options(headers: {"Content-Type" => "application/json"}, debug: true)
-    auth :digest, 'Test', '547e16a6a6a9a9dabe5ce49ac3f1cf03'
+    auth :digest, William.config.app_name, William.config.app_token
 
+    # Public: lists all subscriptions of the application.
+    #
+    # Returns an Array of Hyperclient::Resource
     def list_subscriptions
+      links.subscriptions.reload.embedded.subscriptions
     end
 
-    def create_subscription
+    # Public: Creates a new subscription for the application given the
+    # subscription data.
+    #
+    # subscription - A Hash with subscription data.
+    #   Example:
+    #   {
+    #     periodicity:'annually',
+    #     items:{'centre' => 300, "alumnes" => (30*9)},
+    #     customer:{
+    #       name:'Escola Codegram',
+    #       cif:'b6523423',
+    #       email: 'info@codegram.com',
+    #       address: ['Fake street, 123', 'Terrassa']
+    #     }
+    #   }
+    #
+    # Returns the parsed HTTP response with new subscription data.
+    def create_subscription(subscription)
+      links.subscriptions.post({subscription: subscription}.to_json)
     end
 
-    def show_subscription
+    # Public: Creates a new coupon for the given subscription,
+    #
+    # subscription_id - The subscription that has this coupon.
+    #
+    # coupon - A Hash with coupon data.
+    #   Example:
+    #   {
+    #     "description":"Descompte per referencia",
+    #     "amount":23.45
+    #   }
+    #
+    # Returns the parsed HTTP response with new coupon data.
+    def create_coupon_for(subscription_id, coupon)
+      subscription = get_subscription_resource(subscription_id)
+      subscription.links.coupons.post({coupon: coupon}.to_json)
     end
 
-    def create_coupon
+    # Public: gets last generated invoice for a given subscription.
+    #
+    # subscription_id - The id of the subscription from which retrieve
+    # invoices.
+    #
+    # Returns an Hyperclient::Resource
+    def last_invoice_of(subscription_id)
+      subscription = get_subscription_resource(subscription_id)
+      subscription.links.invoices.embedded.invoices.last
     end
 
-    def show_invoice
+
+    # Public: gets a subscription given its ID.
+    #
+    # subscription_id - The id of the subscription that we want.
+    #
+    # Returns an Hyperclient::Resource
+    def get_subscription_resource(subscription_id)
+      # TODO: Finish Hyperclient gem uri_template branch so we can use the
+      # subscriptions link 'find' to retrieve a single subscription.
+      # The link is already present at subscription's view of the William API.
+      Hyperclient::Resource.new(links['subscriptions'].url.concat("?id=#{subscription_id}"))
     end
 
     # def create_subscription
@@ -30,8 +85,8 @@ module William
     #   c.links.subscriptions.reload.embedded.subscriptions
     #   c.links.subscriptions.post({subscription: abat_oliva}.to_json)
     #   abat_oliva = {periodicity: 'annually', items: {'centre' => 300,
-    #   "alumnes" => (30*9)}, customer: {name: 'Escola ABAT Oliva', cif:
-    #   'B6523423', email: 'divins@codegram.com', address: ['Cacafuti 1']}}
+    #   "alumnes" => (30*9)}, customer: {name: 'escola abat oliva', cif:
+    #   'b6523423', email: 'divins@codegram.com', address: ['cacafuti 1']}}
     #   c.links.subscriptions.reload.embedded.subscriptions.length
     #   c.links.subscriptions.reload.embedded.subscriptions.first.attributes['items']
     # end
