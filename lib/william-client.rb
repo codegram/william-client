bf594512b4f30adde6af2d54e13efaaab577f82d
Marc Divins <marcdivc@gmail.com>
2012-08-27 10:16:00 +0200

    Connection initialized on new

--- lib/william-client.rb
+++ lib/william-client.rb
@@ -6,9 +6,15 @@ module William
   class Client
     include Hyperclient
 
-    entry_point{ William.config.william_api_url }
-    http_options(headers: {"Content-Type" => "application/json"}, debug: true)
-    auth{ {type: :digest, user: William.config.app_name, password: William.config.app_token} }
+    # Public: Initializes the client with the necessary connection to the
+    # service.
+    #
+    # Returns nothing.
+    def initialize
+      self.class.entry_point{ William.config.william_api_url }
+      self.class.http_options(headers: {"Content-Type" => "application/json"}, debug: true)
+      self.class.auth{ {type: :digest, user: William.config.app_name, password: William.config.app_token} }
+    end
 
     # Public: lists all subscriptions of the application.
     #
